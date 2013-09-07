e963b95215dc8d8ed9126f09f50b2f0bd8ff3bee
mtpress <press_martin@bah.com>
2013-09-07 13:11:19 -0400

    In Space!

---
+++ Assets/Starmap/Scripts/GalaxyMap.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class GalaxyMap : MonoBehaviour {
+
+
+	public List<Planet> worlds; 
+
+	private bool Finished;
+	// Use this for initialization
+
+
+
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+
+}
---
+++ Assets/Starmap/Scripts/Planet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour {
+
+	
+	public bool hasMineralRights;
+	public bool mineralRightCost;
+	public string planetNsame; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
