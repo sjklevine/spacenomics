b8dc824623c15ac39c629944400db8ffeaae957d
Andrew Eiche <eiche_andrew@bah.com>
2013-09-07 17:06:19 -0400

    fixing my badness

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -14,6 +14,7 @@ public class Ship : MonoBehaviour {
 	}
 	
 	public bool AddCargo(Blocks b){
+
 		if( cargo.Count >= cargoSpace){
 			return false;
 		}
@@ -23,32 +24,34 @@ public class Ship : MonoBehaviour {
 		text.text = "Cargo " + cargo.Count.ToString("00") + "/50";
 
 		int newCargoAmount = 0;
-
+	
 		switch(b){
 		case Blocks.MoonRocks:
 			newCargoAmount = (int)GameModel.Instance.cargo[GameModel.elementNames[5]] + 1;
-			GameModel.Instance.cargo[commodityString] = newCargoAmount;
+			GameModel.Instance.cargo[GameModel.elementNames[5]] = newCargoAmount;
 			break;
 		case Blocks.Spice:
 			newCargoAmount = (int)GameModel.Instance.cargo[GameModel.elementNames[0]] + 1;
-			GameModel.Instance.cargo[commodityString] = newCargoAmount;	
+			GameModel.Instance.cargo[GameModel.elementNames[0]] = newCargoAmount;	
 			break;
 		case Blocks.Unobtainium:
 			newCargoAmount = (int)GameModel.Instance.cargo[GameModel.elementNames[1]] + 1;
-			GameModel.Instance.cargo[commodityString] = newCargoAmount;	
+			GameModel.Instance.cargo[GameModel.elementNames[1]] = newCargoAmount;	
 			break;
 		case Blocks.OxyAle:
 			newCargoAmount = (int)GameModel.Instance.cargo[GameModel.elementNames[2]] + 1;
-			GameModel.Instance.cargo[commodityString] = newCargoAmount;	
+			GameModel.Instance.cargo[GameModel.elementNames[2]] = newCargoAmount;	
 			break;
 		case Blocks.VesperGas:
 			newCargoAmount = (int)GameModel.Instance.cargo[GameModel.elementNames[3]] + 1;
-			GameModel.Instance.cargo[commodityString] = newCargoAmount;	
+			GameModel.Instance.cargo[GameModel.elementNames[3]] = newCargoAmount;	
 			break;
 		case Blocks.TheFifthElement:
 			newCargoAmount = (int)GameModel.Instance.cargo[GameModel.elementNames[4]] + 1;
-			GameModel.Instance.cargo[commodityString] = newCargoAmount;	
+			GameModel.Instance.cargo[GameModel.elementNames[4]] = newCargoAmount;	
 			break;
+
+		}
 
 		return true;
 	}
