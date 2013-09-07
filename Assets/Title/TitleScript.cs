13e17d35a7e136534af1d58f59935a34c9106e0f
Sam Levine <samuel.levine@gmail.com>
2013-09-07 17:40:51 -0400

    End-of-hackathon presentation build!

--- Assets/Title/TitleScript.cs
+++ Assets/Title/TitleScript.cs
@@ -6,6 +6,7 @@ public class TitleScript : MonoBehaviour {
 
 
 	void Start () {
+		new GameModel();
 		startRect = new Rect(Screen.width*0.4f, Screen.height*0.78f, Screen.width*0.2f, Screen.height*0.15f);
 	}
 	
